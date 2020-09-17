85f4a83592d3e7ba83754484ffb66fe56b0bf680
Nprime496 <nnanejunior@gmail.com>
2020-09-17 10:31:00 +0100

    t

--- src/metrics.py
+++ src/metrics.py
@@ -59,6 +59,7 @@ def coco(sent_a, sent_b, ref_5, ref_10, ref_20, corruption, f,metrics,showoff,nu
     if showoff==True:
       coco_results= [coco_accuracy(sent_a, sent_b, ref_20, corruption in m_p,metric,showoff,num)]
       continue
+    
     coco_results= [coco_accuracy(sent_a, sent_b, ref_5, corruption in m_p,metric,showoff,num),
               coco_accuracy(sent_a, sent_b, ref_10, corruption in m_p,metric,showoff,num),
               coco_accuracy(sent_a, sent_b, ref_20, corruption in m_p,metric,showoff,num)]
