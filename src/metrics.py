481d5a447e82ec185c11af38417ad30b0d35233b
Nprime496 <nnanejunior@gmail.com>
2020-09-19 13:11:12 +0100

    t

--- src/metrics.py
+++ src/metrics.py
@@ -152,6 +152,8 @@ def coco_eval(candidates_file, references_file,metric,showoff,num):
     -METEOR
     -CIDEr
   """
+  print("candidates ",candidates_file)
+  print("references ",references_file)
 
   # This is used to suppress the output of coco-eval:
   old_stdout = sys.stdout
